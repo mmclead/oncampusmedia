61c2f526387c06015bb0a9891b20881dc8779f86
Mason McLead <mason@lapshare.com>
2013-05-22 08:18:06 -0700

    requiring user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name, :phone, :title
+  validates_presence_of :email, :name, :phone, :title
   
   has_many :ratecards
   
