a40c7002476317d9d8a6f73006f3d749fec276a6
Mason McLead <mason@lapshare.com>
2013-05-22 01:15:46 -0700

    changing to env_vars

--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -2,5 +2,5 @@ Dropbox::API::Config.app_key    = ENV['DB_APP_KEY']
 Dropbox::API::Config.app_secret = ENV['DB_APP_SECRET']
 Dropbox::API::Config.mode       = "sandbox"
 
-Dropbox_Token = 'j235maiukk0t48w'
-Dropbox_Secret = 'q1z1xbndti4pq07'
+Dropbox_Token = ENV['DB_CLIENT_TOKEN'] 
+Dropbox_Secret = ENV['DB_CLIENT_SECRET'] 
