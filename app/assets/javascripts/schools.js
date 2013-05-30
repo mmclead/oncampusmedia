150fdde6f0e2d9cd563d5f9b7170c843b75a9189
Mason McLead <mason@lapshare.com>
2013-05-30 16:23:11 -0700

    adjust step for enrollment range

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -68,23 +68,14 @@ $(document).ready(function() {
     var DateFilter = [];
 
     $('#text-filter').keyup(function(event) {
-      //if esc is pressed or nothing is entered
       if (event.keyCode == 27 || $(this).val() == '') {
-        //if esc is pressed we want to clear the value of search box
         $(this).val('');
-  			
-        //we want each row to be visible because if nothing
-        //is entered then all rows are matched.
         $('.school-list li').removeClass('visible').show().addClass('visible');
       }
-  
-      //if there is text, lets filter
       else {
         textFilter('.school-list li', $(this).val());
       }
     });
-    
-
     
     $(".demo-range").each(function() {
       var demo = DemoList[$(this).attr('id')]
@@ -92,6 +83,7 @@ $(document).ready(function() {
         range: true,
         min: demo.min,
         max: demo.max,
+        step: (demo.name == "enrollment") ? 100 : 1,
         values: [ demo.min, demo.max ],
         slide: function(event, ui) {
           if (demo.percent) {
@@ -308,7 +300,6 @@ $(document).ready(function() {
       if ($(this).is(':checked')) { addToSelectedList(this) }
       else { removeFromSelectedList(this.value) }
     });
-    
 
     
     var VisibleMarkers = function() {
