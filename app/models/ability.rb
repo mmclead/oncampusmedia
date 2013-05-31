d0aa36615adeda02f6d7c55df6010c9fe11ad3e7
Mason McLead <mason@lapshare.com>
2013-05-31 13:15:40 -0700

    adding admin role to users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,28 +2,23 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
     user ||= User.new # guest user (not logged in)
-      if user.internal?
+      if user.admin?
         can :manage, :all
+      elsif user.internal?
+        can :manage, Ratecard
+        can :manage, School
       elsif user.id.present?
-        puts "ID Present"
         cannot :read, Ratecard.where('user_id != ?', user.id)
         can :read, Ratecard
         can :create, Ratecard
         can :update, Ratecard, :user_id => user.id
         can :read, School
       else
-        puts "Not logged in"
         can :read, Ratecard, user_id: nil
         cannot :read, Ratecard.owned
         can :read, School
         can :create, Ratecard
-
       end
-      
-    
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521235444) do
+ActiveRecord::Schema.define(:version => 20130531200931) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "school_id"
@@ -197,6 +197,7 @@ ActiveRecord::Schema.define(:version => 20130521235444) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
