6d3f821085d63d896d6038d41acb5a58bbcfdfdd
Mason McLead <mason@lapshare.com>
2013-06-02 13:37:43 -0700

    calc trans for specific time period

--- app/models/transactions.rb
+++ app/models/transactions.rb
@@ -15,6 +15,32 @@ class Transactions < ActiveRecord::Base
     total / 52
   end
   
+  def for_period(start_date, end_date)
+    months_between = (end_date.year * 12 + end_date.month) - (start_date.year * 12 + start_date.month)
+    total = 0
+    if months_between > 0
+      total+= self.send(start_date.strftime('%B').downcase) * 
+              ((((Date.new(start_date.year, start_date.next_month.month)-1).mday - start_date.mday)+1).to_f / 
+              (Date.new(start_date.year, start_date.next_month.month)-1).mday).to_f
+      total+= self.send(end_date.strftime('%B').downcase) * 
+              (end_date.mday.to_f / (Date.new(end_date.year, end_date.next_month.month)-1).mday).to_f
+      if months_between > 1
+        this_month = start_date
+        (months_between-1).times do 
+          this_month = this_month.next_month
+          total += self.send(this_month.strftime('%B').downcase)
+        end
+      end
+      
+    else
+      
+      total+= self.send(start_date.strftime('%B').downcase) * 
+              (((end_date.mday - start_date.mday)+1).to_f / 
+              (Date.new(start_date.year, start_date.next_month.month)-1).mday).to_f
+    end
+    return total
+  end
+  
   private
   
   def update_total
