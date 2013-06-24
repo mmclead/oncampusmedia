d42ce96ccd72a488cd48355cd1ed094e92377593
Mason McLead <mason@lapshare.com>
2013-06-24 12:28:55 -0700

    modifying settings for pdfs

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,10 +1,17 @@
 if Rails.env.staging? || Rails.env.production?
   WickedPdf.config = {
-    :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+    :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
+    :dpi => '300',
+    :orientation => 'Portrait',
+    :page_size  => "Letter",
+    :page_height => '5in', 
+    :page_width => '7in'
   }
 else
   WickedPdf.config = {
-    :exe_path => '/usr/local/bin/wkhtmltopdf'
+    :exe_path => '/usr/local/bin/wkhtmltopdf',
+    :dpi => '300',
+    :page_size  => "Letter",
   }
 end
   
