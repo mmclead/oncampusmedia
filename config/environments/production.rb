4cd59d90980b55d914044f83a6bfd599397f28f1
Mason McLead <mason@lapshare.com>
2013-10-27 11:50:04 -0700

    changing prod default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Schools::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'proposals.on-campusmedia.com' }
+  config.action_mailer.default_url_options = { :host => 'on-campusmedia.com' }
   
   # Enable threaded mode
   # config.threadsafe!
