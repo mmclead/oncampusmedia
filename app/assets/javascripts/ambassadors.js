2bcb6f4e29c3e3e2964f244b96b066b77faba44b
Mason McLead <mason@lapshare.com>
2014-01-18 23:14:36 -0800

    moving ambassador creation to its new page

--- app/assets/javascripts/ambassadors.js
+++ app/assets/javascripts/ambassadors.js
@@ -1,4 +1,16 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(document).ready(function() {
 
+  $('.ambassador_schools input, .school_ambassadors_schools input').change(function() {
+    if(this.checked) {
+      $(this.parentElement).appendTo($('#current_ambassador_schools .controls'));
+    }
+    else {
+     $(this.parentElement).prependTo($('#non_ambassador_schools .controls')); 
+    }
+  });
+
+  if($('#included_school_id').val()) {
+    $("#ambassador_school_ids_"+$('#included_school_id').val()).attr('checked',true);
+    $("#ambassador_school_ids_"+$('#included_school_id').val()).trigger( "change" );
+  }
+});
